0afb82f2568fed0182ed34ad12d4477ddb882c1a
Henry Zwart <henry.zwart@dragonfly.co.nz>
2026-01-07 11:36:32 +0100

    start extracting climate attitudes data

---
+++ scripts/extract_ca_data.r
@@ -0,0 +1,20 @@
+# Prefer to do this in docker
+install.packages("data.table", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("dotenv", repos="https://mirrors.evoluso.com/CRAN/")
+
+library(data.table)
+library(dotenv)
+
+# Load data
+ca_dir <- paste(Sys.getenv("ASSETS_DIR"), "climate-attitudes", sep="/")
+load(paste(ca_dir, "w1w2w3w4w5_indices_weights_jul12_2022.Rdata", sep="/"))
+load(paste(ca_dir, "w6_cleaned_weights_june12_2023.Rdata", sep="/"))
+
+# Data from waves 1 -- 5 loads as 'df'
+w1_to_w5 <- df
+
+# Convert to data.table
+setDF(w1_to_w5)
+setDF(w6)
+
+head(w6)
