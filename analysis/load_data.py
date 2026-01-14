11efbb0111adb07e81777c2a82545bf03298d917
Henry Zwart <henry.zwart@dragonfly.co.nz>
2026-01-14 19:01:54 +0100

    extract rdata to parquet

---
+++ analysis/load_data.py
@@ -0,0 +1,12 @@
+import polars as pl
+
+
+def main():
+    df = pl.read_parquet(
+        "~/data/msc_thesis/climate-attitudes/w1w2w3w4w5_indices_weights_jul12_2022.parquet"
+    )
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
