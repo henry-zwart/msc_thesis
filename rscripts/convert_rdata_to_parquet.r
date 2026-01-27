f4ddb575f12a03cba82618e3698898afa6c7c853
Henry Zwart <henry.zwart@dragonfly.co.nz>
2026-01-27 17:53:51 +0100

    add notebooks, scripts

---
+++ rscripts/convert_rdata_to_parquet.r
@@ -0,0 +1,12 @@
+library(arrow)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+print(args)
+
+# Load data
+var_name <- load(args[1])
+
+# Save as parquet
+write_parquet(get(var_name), args[2])
+
---
+++ scripts/within_vs_between_person_correlation.py
@@ -0,0 +1,45 @@
+"""Example of problem inferring within-person belief system from cross-sectional data."""
+
+import numpy as np
+
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+
+def main():
+    """Consider two beliefs. Make these be positively correlated in the population, but
+    negatively correlated within each person.
+
+    To be positively correlated in population, must have that generally A iff B.
+
+    To be negatively correlated within people, must have that for each person, generally
+    A iff ~B.
+    """
+    rng = np.random.default_rng(161020)
+
+    # First, consider beliefs shared by majority of population, which fluctuate over time
+    #   - At any point in time they have positive correlation in population
+    #   - Over time they have zero correlation for each individual
+    n = 10000
+    timesteps = 2000
+    ideology = rng.binomial(1, 0.5, size=n)
+    b1 = rng.binomial(1, (1 - ideology) * 0.3 + ideology * 0.7, size=(timesteps, n))
+    b2 = rng.binomial(1, (1 - ideology) * 0.3 + ideology * 0.7, size=(timesteps, n))
+    within_corrs = stats.pearsonr(b1, b2, axis=0).statistic
+    between_corrs = stats.pearsonr(b1, b2, axis=1).statistic
+
+    fig, axe = plt.subplots(figsize=(5, 3), constrained_layout=True)
+    axe.hist(within_corrs, density=True, alpha=0.7, label="Within-person")
+    axe.hist(between_corrs, density=True, alpha=0.7, label="Between-person")
+    axe.set_xlabel("Correlation")
+    axe.set_ylabel("Probability density")
+    axe.spines["top"].set_visible(False)
+    axe.spines["right"].set_visible(False)
+    axe.set_xlim(-0.1, 0.25)
+    axe.legend()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
