7abdf1d9c36aa16c329858e5d6a67ef30dd92ee9
Henry Zwart <henry.zwart@dragonfly.co.nz>
2026-03-17 11:42:20 +0100

    add wtp variables; refactor dataset specification to separate input from output (transformed) variables

--- src/climate_attitudes/datasets/imputed.py
+++ src/climate_attitudes/datasets/imputed.py
@@ -6,6 +6,109 @@ SURVEY_COLS = [
     "wave",
 ]
 
+INPUT_QUESTION_COLUMNS = [
+    "cc1",
+    "cc2",
+    "cc4_world",
+    "cc4_wealthUS",
+    "cc4_poorUS",
+    "cc4_comm",
+    "cc5_world",
+    "cc5_wealthUS",
+    "cc5_poorUS",
+    "cc5_comm",
+    "cc10",
+    "cc11",
+    "cc12",
+    "cvcc4_will",
+    "soc_trust",
+    "soc_help",
+    "future",
+    "cvcc10_cc",
+    "cc3",
+    "cc6",
+    # "ccIO",
+    # "ccGovt",
+    "ccSolving",
+    "ccCompensation",
+    "cvcc_worryothers",
+    "ew5",
+    "cvcc4_should",
+    "cvcc6",
+    "cvcc9_econ",
+    "cvcc9_cc",
+    "cvcc9_cv",
+    "cc_ica",
+    "cc_pol_tax",
+    "cc_pol_car",
+    "pol4",
+    "pol7",
+    "pol7_pi",
+    "pol8",
+    "pol8_pi",
+    "pol9",
+    "pol10",
+    "pol11",
+    "pol11_pi",
+    "pol_affiliation",
+    "pol_ideology",
+    "pol_worry_econ",
+    "pol_trust_cong",
+    "pol_trust_state",
+    "pol_trust_io",
+    "pol_trust_cdc",
+    "pol_trust_news",
+    "pol_trust_epa",
+    "pol_trust_sci",
+    "pol_laws_cong",
+    "pol_laws_state",
+    # "pol_vote_support",
+    "ew6",
+    "cvcc4_personal",
+    "dem_age",
+    "dem_income",
+    "dem_urban",
+    "current_pres",
+    "Variant_ccSolving",
+    "Variant_ccCompensation",
+]
+
+ALL_INPUT_COLUMNS = SURVEY_COLS + INPUT_QUESTION_COLUMNS
+
+REVERSE_CODING = [
+    "cc10",
+    "pol4",
+    "pol8",
+    "pol8_pi",
+    "pol9",
+    "pol10",
+    "pol11",
+    "pol11_pi",
+    "dem_urban",
+]
+
+TRANSFORMS = [
+    pl.col("cc1").replace({1: 2, 99: 1}),  # Move "yes" to 2, "don't know" to 1
+    pl.col(r"^cc4_(world|wealthUS|poorUS|comm)$").replace(
+        {1: 0, 2: 1, 99: 2}
+    ),  # Shift "not at all", "only a little" down; insert "don't know" between "only a little" and "a moderate amount"
+    pl.col(r"^cc5_(world|wealthUS|poorUS|comm)$").replace({1: 0, 2: 1, 99: 2}),
+]
+
+VITERBI_IMPUTE_COLS = [
+    col
+    for col in INPUT_QUESTION_COLUMNS
+    if col
+    not in (
+        "dem_age",
+        "ccSolving",
+        "ccCompensation",
+        "Variant_ccSolving",
+        "Variant_ccCompensation",
+    )
+]
+FILL_IMPUTE_COLS = pl.col("dem_age")
+
 BELIEF_COLS = [
     "cc1",
     "cc2",
@@ -34,6 +137,8 @@ ATTITUDE_COLS = [
     "cc6",
     # "ccIO",
     # "ccGovt",
+    "wtp_solve",
+    "wtp_compensation",
     "cvcc_worryothers",
     "ew5",
     "cvcc4_should",
@@ -74,8 +179,6 @@ BEHAVIOUR_COLS = [
 ]
 
 DEMOGRAPHIC_COLS = [
-    # "dem_age",
-    "dem_income",
     "dem_urban",
 ]
 
@@ -87,25 +190,6 @@ TREATMENT_COLUMNS = [
     # "Group_pol_vote_support",
 ]
 
-REVERSE_CODING = [
-    "cc10",
-    "pol4",
-    "pol8",
-    "pol8_pi",
-    "pol9",
-    "pol10",
-    "pol11",
-    "pol11_pi",
-    "dem_urban",
-]
-
-TRANSFORMS = [
-    pl.col("cc1").replace({1: 2, 99: 1}),  # Move "yes" to 2, "don't know" to 1
-    pl.col(r"^cc4_(world|wealthUS|poorUS|comm)$").replace(
-        {1: 0, 2: 1, 99: 2}
-    ),  # Shift "not at all", "only a little" down; insert "don't know" between "only a little" and "a moderate amount"
-    pl.col(r"^cc5_(world|wealthUS|poorUS|comm)$").replace({1: 0, 2: 1, 99: 2}),
-]
 
 QUESTION_COLS = (
     BELIEF_COLS
@@ -116,7 +200,7 @@ QUESTION_COLS = (
     + EXTERNAL_FACTORS
 )
 
-ALL_COLS = SURVEY_COLS + QUESTION_COLS + TREATMENT_COLUMNS
+ALL_COLS = SURVEY_COLS + QUESTION_COLS
 
 CATEGORIES = np.asarray(
     ["Belief"] * len(BELIEF_COLS)
--- src/climate_attitudes/datasets/imputed_reduced.py
+++ src/climate_attitudes/datasets/imputed_reduced.py
@@ -6,6 +6,76 @@ SURVEY_COLS = [
     "wave",
 ]
 
+INPUT_QUESTION_COLUMNS = [
+    "cc1",
+    "cc2",
+    "cc4_world",
+    "cc4_wealthUS",
+    "cc4_poorUS",
+    "cc4_comm",
+    "cc5_world",
+    "cc5_wealthUS",
+    "cc5_poorUS",
+    "cc5_comm",
+    "cc10",
+    "cc11",  # If included, cc10 and cc12 related. If excluded, they are less so. Possible collider.
+    "cc12",
+    "cvcc4_will",
+    "cc3",
+    "cc6",
+    # "ccIO",
+    # "ccGovt",
+    "ccSolving",
+    "ccCompensation",
+    "cvcc_worryothers",
+    "ew5",
+    "cvcc4_should",
+    "cvcc6",
+    "cvcc9_cc",
+    "cc_ica",
+    "cc_pol_tax",
+    "cc_pol_car",
+    "pol7",
+    "pol7_pi",
+    "pol_affiliation",
+    "pol_ideology",
+    "ew6",
+    "cvcc4_personal",
+    "dem_age",
+    "dem_income",
+    "Variant_ccSolving",
+    "Variant_ccCompensation",
+]
+
+ALL_INPUT_COLUMNS = SURVEY_COLS + INPUT_QUESTION_COLUMNS
+
+REVERSE_CODING = [
+    "cc10",
+]
+
+TRANSFORMS = [
+    pl.col("cc1").replace({1: 2, 99: 1}),  # Move "yes" to 2, "don't know" to 1
+    pl.col(r"^cc4_(world|wealthUS|poorUS|comm)$").replace(
+        {1: 0, 2: 1, 99: 2}
+    ),  # Shift "not at all", "only a little" down; insert "don't know" between "only a little" and "a moderate amount"
+    pl.col(r"^cc5_(world|wealthUS|poorUS|comm)$").replace({1: 0, 2: 1, 99: 2}),
+]
+
+VITERBI_IMPUTE_COLS = [
+    col
+    for col in INPUT_QUESTION_COLUMNS
+    if col
+    not in (
+        "dem_age",
+        "ccSolving",
+        "ccCompensation",
+        "Variant_ccSolving",
+        "Variant_ccCompensation",
+    )
+]
+FILL_IMPUTE_COLS = pl.col("dem_age")
+
+
 BELIEF_COLS = [
     "cc1",
     "cc2",
@@ -18,12 +88,9 @@ BELIEF_COLS = [
     "cc5_poorUS",
     "cc5_comm",
     "cc10",
-    "cc11",
+    "cc11",  # If included, cc10 and cc12 related. If excluded, they are less so. Possible collider.
     "cc12",
     "cvcc4_will",
-    # "soc_trust",
-    # "soc_help",
-    "cvcc10_cc",
 ]
 
 EXPERIENCE_COLS = []
@@ -33,34 +100,20 @@ ATTITUDE_COLS = [
     "cc6",
     # "ccIO",
     # "ccGovt",
+    "wtp_solve",
+    "wtp_compensation",
     "cvcc_worryothers",
     "ew5",
     "cvcc4_should",
     "cvcc6",
-    "cvcc9_econ",
     "cvcc9_cc",
-    "cvcc9_cv",
     "cc_ica",
     "cc_pol_tax",
     "cc_pol_car",
-    "pol4",
     "pol7",
     "pol7_pi",
-    "pol8",
-    "pol8_pi",
-    "pol9",
     "pol_affiliation",
     "pol_ideology",
-    "pol_trust_cong",
-    "pol_trust_state",
-    "pol_trust_io",
-    "pol_trust_cdc",
-    "pol_trust_news",
-    "pol_trust_epa",
-    "pol_trust_sci",
-    "pol_laws_cong",
-    # "pol_laws_state",
-    # "pol_vote_support",
 ]
 
 BEHAVIOUR_COLS = [
@@ -70,31 +123,13 @@ BEHAVIOUR_COLS = [
 
 DEMOGRAPHIC_COLS = [
     # "dem_age",
+    # "dem_income",
 ]
 
 EXTERNAL_FACTORS = [
     # "current_pres",
 ]
 
-TREATMENT_COLUMNS = [
-    # "Group_pol_vote_support",
-]
-
-REVERSE_CODING = [
-    "cc10",
-    "pol4",
-    "pol8",
-    "pol8_pi",
-    "pol9",
-]
-
-TRANSFORMS = [
-    pl.col("cc1").replace({1: 2, 99: 1}),  # Move "yes" to 2, "don't know" to 1
-    pl.col(r"^cc4_(world|wealthUS|poorUS|comm)$").replace(
-        {1: 0, 2: 1, 99: 2}
-    ),  # Shift "not at all", "only a little" down; insert "don't know" between "only a little" and "a moderate amount"
-    pl.col(r"^cc5_(world|wealthUS|poorUS|comm)$").replace({1: 0, 2: 1, 99: 2}),
-]
 
 QUESTION_COLS = (
     BELIEF_COLS
@@ -105,7 +140,8 @@ QUESTION_COLS = (
     + EXTERNAL_FACTORS
 )
 
-ALL_COLS = SURVEY_COLS + QUESTION_COLS + TREATMENT_COLUMNS
+
+ALL_COLS = SURVEY_COLS + QUESTION_COLS
 
 CATEGORIES = np.asarray(
     ["Belief"] * len(BELIEF_COLS)
